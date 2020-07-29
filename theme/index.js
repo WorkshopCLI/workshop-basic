f915fb8410986a1f4b40eb1b5e0f6392f0f4b379
salexzee <510samwebb@gmail.com>
2020-07-29 00:13:47 -0700

    removes `new` command since it produces a bunch of files

--- theme/index.js
+++ theme/index.js
@@ -1,13 +1,1 @@
-const themekit = require('@shopify/themekit')
-const yaml = require('js-yaml')
-const fs = require('fs')
-
-module.exports.new = () => {
-  const doc = yaml.safeLoad(fs.readFileSync('./config.yml', 'utf8'))
-
-  themekit.command('new', {
-    password: doc.development.password,
-    store: doc.development.store,
-    name: process.argv[1] || 'Workshop Basic Theme'
-  })
-}
+// Here for future use
